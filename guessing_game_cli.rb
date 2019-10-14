9d1eab55262ed7296dc44ac984df33c1822b21ba
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-14 16:43:55 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,10 +3,10 @@ def run_guessing_game
   puts "Pick a number 1-6"
   guess = gets.chomp
   number = rand(6) + 1
-  if guess != number
-    puts "Sorry! The computer guessed #{number}."
-  elsif guess == "exit"
+  if guess == "exit"
     puts "Goodbye!"
+  elsif guess != number
+    puts "Sorry! The computer guessed #{number}."
   elsif guess == number
     puts "You guessed the correct number!"
   end
